b15eee5a1e942c5333c47a40f48976e9895d5ebf
luiscaputo <luiscaputo15@gmail.com>
2021-07-01 00:04:23 +0100

    :construction: form-register-updated

--- index.php
+++ index.php
@@ -47,29 +47,46 @@
     }
   }
   }
-if(isset($_POST['register_n']))
+if(isset($_POST['gerar']))
 {
     $nome = filter_input(INPUT_POST, 'name');
-    if($nome == ''){
-      echo "<script>alert('Não pode se cadastrar sem nome!')</script>";
+    $birth = filter_input(INPUT_POST, 'year');
+    $province = filter_input(INPUT_POST, 'province');
+    if($nome == '' || empty($birth) || empty($province)){
+      echo "<script>alert('Por favor todos devem estar preenchidos!')</script>";
     }else
       {
-        $div = explode(' ', $nome);
-        $last = end($div);
-        $F = $nome[0];
-        $l = $last[0];
-        $gera = rand(1000, 9999);
-        $data = date('y');
-        $codV = $F.$l.$gera.$data;
-        
-        $save = $pdo->prepare("INSERT INTO voters(name, codVoter) VALUES ('$nome', '$codV')");
-        $save->execute();
+        $data = '20'.date('y');
+        $p1 = $birth[0];
+        $p2 = $birth[1];
+        $p3 = $birth[2];
+        $p4 = $birth[3];
 
-        if($save->rowCount() > 0){
-          echo "<script>alert('Sr. `{$nome}` seu cadastro foi feito com sucesso, seu código de voto é `{$codV}` Anote-o ou faça uma captura de ecrã! E leve ele consigo para validar o seu voto. Lembra, votar é direito de todos!')</script>";
-        }else
-        {
-          echo 'BUG - Try Again Later';
+        $dt = $p1.$p2.$p3.$p4;
+        $yearBirth = $data - $dt; 
+        if($dt >= $data){
+          echo "<script>alert('Data de Nascimento inválida! Verifique e tente novamente.')</script>";
+        }else{
+          if($yearBirth < 18){
+            echo "<script>alert('Você ainda é menor de Idade e não pode Votar!')</script>";
+          }else{
+            $div = explode(' ', $nome);
+            $last = end($div);
+            $F = $nome[0];
+            $l = $last[0];
+            $gera = rand(1000, 9999);
+            $codV = $F.$l.$gera.$data;
+            
+            $save = $pdo->prepare("INSERT INTO voters(name, codVoter) VALUES ('$nome', '$codV')");
+            $save->execute();
+
+            if($save->rowCount() > 0){
+              echo "<script>alert('Sr. `{$data}` seu cadastro foi feito com sucesso, seu código de voto é `{$codV}` Anote-o ou faça uma captura de ecrã! E leve ele consigo para validar o seu voto. Lembra, votar é direito de todos!')</script>";
+            }else
+            {
+              echo 'BUG - Try Again Later';
+            }
+          }
         }
       }
   }
@@ -92,15 +109,39 @@ if(isset($_POST['register_n']))
     <form method="post" id="regi" class="form-control text-justify p-3 mt-13" style="color: white; background-color:#323238; border:none; width:48%; margin-left:25%;">
       <strong class="text-center">CADASTRE-SE AQUI - PREENCHA TODOS OS CAMPOS</strong>
       <hr>
-      <div class="mb-3">
+      <div class="mb-0">
         <label for="exampleInputEmail1" class="form-label"></label>
         <input type="text" class="form-control" id="nome" name="name" aria-describedby="Nome" placeholder="Nome Completo">
       </div>
+      <div class="mb-3">
+        <label for="yearOld" class="form-label"></label>
+        <input type="date" class="form-control" name="year">
+      </div>
       <div class="input-group mb-3">
         <label for="exampleInputEmail1" class="form-label"></label>
-        <input type="text" class="form-control mw-50" id="bi" name="bi" aria-describedby="Nome" placeholder="NIF ou Código">
+        <select name="province" id="" class="form-control">
+          <option value="" selected>Selecione sua província</option>
+          <option value="Luanda">Luanda</option>
+          <option value="Lunda-Sul">Lunda-Sul</option>
+          <option value="Lunda-Norte">Lunda-Norte</option>
+          <option value="Kwanza-Sul">Kwanza-Sul</option>
+          <option value="Kwanza-Norte">Kwanza-Norte</option>
+          <option value="Moxico">Moxico</option>
+          <option value="Bengo">Bengo</option>
+          <option value="Uíge">Uíge</option>
+          <option value="Huíla">Huíla</option>
+          <option value="Benguela">Benguela</option>
+          <option value="Namibe">Namibe</option>
+          <option value="Cunene">Cunene</option>
+          <option value="Cuando-Cubango">Cuando-Cubango</option>
+          <option value="Bié">Bié</option>
+          <option value="Malange">Malange</option>
+          <option value="Cabinda">Cabinda</option>
+          <option value="Zaire">Zaire</option>
+          <option value="Huambo">Huambo</option>
+        </select>
       </div>
-      <button type="submit" class="btn btn-success form-control" id="register_bi" name="register_bi">Gerar Meu Código Eleitoral</button><br><br>
+      <button type="submit" class="btn btn-success form-control" id="register_bi" name="gerar">Gerar Meu Código Eleitoral</button><br><br>
     </form>
   </div>
 </div>
